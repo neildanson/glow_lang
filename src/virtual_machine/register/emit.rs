e8275761abc40285bada53c19a8209e56bdd5bcb
neil <neil.danson@gmail.com>
2023-01-14 19:52:27 +0000

    some implementation and passing tests for register vm

--- src/virtual_machine/register/emit.rs
+++ src/virtual_machine/register/emit.rs
@@ -1,12 +1,66 @@
+use std::collections::{HashMap, BTreeMap};
+
 use super::vm::*;
 use crate::{interface::Compiler, *};
 
-pub fn emit(expr: &Expr) -> Vec<Instruction> {
-    vec![]
+pub fn extract_value(expr:&Expr) -> Option<Values> {
+    match expr {
+        Expr::Int(i) => Some(Values::Int(*i)),
+        Expr::Str(s) => Some(Values::String(s.clone())),
+        _ => None,
+    }
 }
 
-pub fn emit_body(expr: &[Expr]) -> Vec<Instruction> {
-    vec![]
+pub fn emit(expr: &Expr, locals : &mut HashMap<String, usize>, locals_rev:&mut BTreeMap<usize, String>) -> Vec<Instruction> {
+    let mut instructions = Vec::new();
+    match expr {
+        Expr::Ident(name, expr) => {
+            //See if name already has an index
+            let index = 
+                if let Some(index) = locals.get(name) {
+                    *index
+                } else {
+                    let i = 
+                        match locals_rev.last_entry() { 
+                            Some(x) => *x.key() + 1,
+                            None => 0,
+                        };
+                    locals_rev.insert(i, name.clone());
+                    locals.insert(name.clone(),i);
+                    i
+                };
+            
+            instructions.push(Instruction::StoreLocal(index, extract_value(&expr).unwrap()));
+        },
+        Expr::Add(l,r) => {
+            let l = *l.clone();
+            let r = *r.clone();
+            match (l,r) {
+                (Expr::Symbol(l), Expr::Symbol(r)) => {
+                    let l = locals.get(&l).unwrap();
+                    let r = locals.get(&r).unwrap();
+                    instructions.push(Instruction::Add(*l, *r))
+
+                },
+                _ => {},
+            }
+        }
+        _ => {},
+    }
+    instructions
+}
+
+pub fn emit_body(exprs: &[Expr]) -> Vec<Instruction> {
+    let mut locals :HashMap<String, usize>= HashMap::new();
+    let mut locals_rev :BTreeMap<usize, String>= BTreeMap::new();
+    
+    let mut instructions = Vec::new();
+    let mut i = 0;
+    for e in exprs {
+        let mut instructions_to_add = emit(e, &mut locals, &mut locals_rev);
+        instructions.append(&mut instructions_to_add);
+    }
+    instructions
 }
 
 mod tests {
@@ -14,8 +68,8 @@ mod tests {
 
     #[test]
     fn emit_assign() {
-        let expr = Expr::Ident("x".to_string(), Box::new(Expr::Int(1)));
-        let result = emit(&expr);
+        let expr = vec![Expr::Ident("x".to_string(), Box::new(Expr::Int(1)))];
+        let result = emit_body(&expr);
 
         let expected = vec![Instruction::StoreLocal(0, Values::Int(1))];
 
